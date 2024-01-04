9913a20f15be7e26de559b4552bb0bb1ae95997d
kmindev <kangkm0324@gmail.com>
2024-01-04 23:25:43 +0900

    #42 - user-id를 PK로 변경함에 따라 모든 자바코드 수정

--- project-board/src/main/java/com/projectboard/repostiory/UserAccountRepository.java
+++ project-board/src/main/java/com/projectboard/repostiory/UserAccountRepository.java
@@ -3,5 +3,5 @@ package com.projectboard.repostiory;
 import com.projectboard.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }

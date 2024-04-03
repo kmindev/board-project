ce68f3c77662ed523188b1af25ee5c88d732470b
kmindev <kangkm0324@gmail.com>
2024-04-03 22:36:05 +0900

    #41 - BoardPrincipal: OAuth 인증 정보를 다룰 수 있도록 기능 추가 `OAuth2User` 인터페이스를 구현해서 Spring OAuth Client 도 기존과 동일하게 인증 정보를 인식할 수 있게끔 함

--- project-board/src/main/java/com/projectboard/dto/security/BoardPrincipal.java
+++ project-board/src/main/java/com/projectboard/dto/security/BoardPrincipal.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,11 +18,15 @@ public record BoardPrincipal(
         String password,
         String email,
         String nickname,
-        String memo
-) implements UserDetails {
-
+        String memo,
+        Map<String, Object> oAuth2Attributes
+) implements UserDetails, OAuth2User {
 
     public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
+        return of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo, Map<String, Object> oAuth2Attributes) {
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
         return new BoardPrincipal(
@@ -32,7 +38,8 @@ public record BoardPrincipal(
                 password,
                 email,
                 nickname,
-                memo
+                memo,
+                oAuth2Attributes
         );
     }
 
@@ -89,6 +96,14 @@ public record BoardPrincipal(
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() { return oAuth2Attributes; }
+
+    @Override
+    public String getName() {
+        return username;
     }
 
     public enum RoleType {
